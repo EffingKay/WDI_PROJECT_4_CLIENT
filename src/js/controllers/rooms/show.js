b9873f90dd63f5d6e11f74d1e9e4fa3b898aae1a
EffingKay <rozgonyiova.k@gmail.com>
2017-02-20 10:18:03 +0000

    basic game created - no logic

--- src/js/controllers/rooms/show.js
+++ src/js/controllers/rooms/show.js
@@ -3,12 +3,45 @@ angular
   .controller('RoomsShowCtrl', RoomsShowCtrl);
 
 RoomsShowCtrl.$inject = ['API', '$http', '$stateParams', '$resource'];
+
 function RoomsShowCtrl(API, $http, $stateParams, $resource) {
   const vm     = this;
+  const Room   = $resource(`${API}/rooms/:id`, { id: '@id' });
+
   vm.room = {};
-
-  const Room = $resource(`${API}/rooms/:id`, { id: '@id' });
   vm.room = Room.get($stateParams);
+  vm.cardsGenerator = cardsGenerator;
+  vm.whiteCards = [];
+  vm.pickedWhiteCards = [];
+  vm.pickWhiteCard = pickWhiteCard;
 
+  const randomNumberGenerator = (array) => {
+    const randomNumber = Math.floor(Math.random() * (array.length + 1));
+    return randomNumber;
+  };
+
+  function cardsGenerator() {
+    blackCardGenerator();
+    whiteCardsGenerator();
+  }
+
+  function blackCardGenerator() {
+    const index = randomNumberGenerator(window.cards.blackCards);
+    vm.blackCard =  window.cards.blackCards[index];
+    return vm.blackCard;
+  }
+
+  function whiteCardsGenerator() {
+    vm.whiteCards = [];
+    for (let i= 0; i<10; i++) {
+      const index = randomNumberGenerator(window.cards.whiteCards);
+      vm.whiteCards.push(window.cards.whiteCards[index]);
+    }
+  }
+
+  function pickWhiteCard($index) {
+    vm.pickedWhiteCards = [];
+    vm.pickedWhiteCards.push(vm.whiteCards[$index]);
+  }
 
 }
